450d3d6eb28afa1f0d489c2705730b843a9d8b92
Alicia Key <alicia.key@galvanize.com>
2017-01-05 15:01:46 -0700

    Hello world works

--- events-form-validation/client/demo-form/demo-form.component.js
+++ events-form-validation/client/demo-form/demo-form.component.js
@@ -10,10 +10,19 @@
     var vm = this;
 
     vm.submitUsername = submitUsername;
+    vm.resetUsernameForm = resetUsernameForm;
     vm.user = {};
 
-    function submitUsername(username) {
-      console.log(username);
+    function submitUsername(username, password) {
+      if (username && password) {
+        console.log(username, password);
+      }
+    }
+
+    function resetUsernameForm() {
+      vm.user = {};
+      vm.usernameForm.$setPristine();
+      vm.usernameForm.$setUntouched();
     }
   }
 })();
