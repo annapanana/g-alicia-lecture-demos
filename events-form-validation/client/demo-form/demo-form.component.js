7c2ec677608a6b35de093e96460a7451734be991
Alicia Key <alicia.key@galvanize.com>
2017-01-05 14:57:30 -0700

    Hello world works

--- events-form-validation/client/demo-form/demo-form.component.js
+++ events-form-validation/client/demo-form/demo-form.component.js
@@ -2,6 +2,18 @@
   angular
     .module("app", [])
     .component("demoForm", {
-      templateUrl: "demo-form/demo-form.template.html"
+      templateUrl: "demo-form/demo-form.template.html",
+      controller: DemoFormController
     });
+
+  function DemoFormController() {
+    var vm = this;
+
+    vm.submitUsername = submitUsername;
+    vm.user = {};
+
+    function submitUsername(username) {
+      console.log(username);
+    }
+  }
 })();
