103875253d7fc5ad0f4022d3f7a1f073a8a71091
Alicia Key <alicia.key@galvanize.com>
2017-01-11 12:11:01 -0700

    Boom! All the routing works now

--- ui-router-basics/home/home.component.js
+++ ui-router-basics/home/home.component.js
@@ -1,18 +1,21 @@
 (function () {
   angular
-    .module("home.component", [])
+    .module("home.component", ["ui.router"])
     .component("home", {
       templateUrl: "/home/home.template.html",
       controller: controller
     });
 
-  function controller() {
+  controller.$inject = ["$state"];
+
+  function controller($state) {
     var vm = this;
 
     vm.submitPhoneNumber = submitPhoneNumber;
 
     function submitPhoneNumber(phoneNumber) {
       console.log(phoneNumber);
+      $state.go("callMe", {phoneNumber: phoneNumber});
     }
   }
 })();
