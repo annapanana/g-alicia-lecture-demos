1e011f430247de199ddc56491a362b42f27f3899
Alicia Key <alicia.key@galvanize.com>
2017-01-11 10:48:13 -0700

    Now it runs the state changes

--- ui-router-basics/app.js
+++ ui-router-basics/app.js
@@ -1,4 +1,23 @@
 (function () {
   angular
-    .module("app", []);
+    .module("app", ["ui.router", "home.component", "about.component"])
+    .config(config);
+
+  config.$inject = ["$stateProvider", "$urlRouterProvider", "$locationProvider"];
+
+  function config($stateProvider, $urlRouterProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
+    $stateProvider
+      .state({
+        name: "home",
+        url: "/",
+        component: "home"
+      })
+      .state({
+        name: "about",
+        url: "/about",
+        component: "about"
+      });
+  }
 })();
